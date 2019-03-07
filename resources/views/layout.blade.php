f9c92e64dd2d746f6c29c64a9af526272a153383
Sebastiaan Prins <sebastiaanprins1@gmail.com>
2019-03-07 09:53:37 +0100

    Bootstrap CDN style ref. for deployment

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>sTravel</title>
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+
+{{-- DEFAULT for CSS; style not working when deployed --}}
+  {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" /> --}}
+
+  <!-- BOOTSTRAP -->
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"> </script>
+
 </head>
 <body>
 
